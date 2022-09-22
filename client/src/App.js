2eaf30b0568e10392898eb4ddbc88e7da6203e3b
jmeegan2 <jmeegan8@gmail.com>
2022-09-21 20:00:25 -0400

    done for today, working on frontend still

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 
+import { useState } from 'react';
 import io from 'socket.io-client'
 
 const socket = io.connect("http://localhost:3001");
 
 function App() {
-  return <h1>Hello</h1>
+  const [username, setUsername] = useState("");
+
+  return (
+  
+    <>
+    <div className='App'>
+    <h3>Join Chat</h3>
+    <input type="text" placeholder="John..." />
+    <input type="text" placeholder='Room ID...'/>
+    <button>Join A Room</button>
+    </div>
+    </>
+  )
 }
 
 export default App;
