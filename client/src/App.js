e6758d2a56337ff49d296081f7eb31a02992d972
jmeegan2 <jmeegan8@gmail.com>
2022-09-22 21:03:28 -0400

    able to console.log user joining room

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,27 @@ const socket = io.connect("http://localhost:3001");
 
 function App() {
   const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
 
+  const joinRoom = () => {
+    if (username !== "" && room !== "" ) {
+      socket.emit("join_room", room)
+    }
+  }
   return (
   
     <>
     <div className='App'>
     <h3>Join Chat</h3>
-    <input type="text" placeholder="John..." />
-    <input type="text" placeholder='Room ID...'/>
-    <button>Join A Room</button>
+    <input type="text" placeholder="John..." onChange={(event) => {
+      setUsername(event.target.value);
+      }}
+       />
+    <input 
+    type="text" placeholder='Room ID...' onChange={(event) => {
+      setRoom(event.target.value);
+      }} />
+    <button onClick={joinRoom}>Join A Room</button>
     </div>
     </>
   )
--- server/index.js
+++ server/index.js
@@ -18,6 +18,12 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
 
+
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined room: ${data}`)
+    })
+
     socket.on("disconnect", () => {
         console.log("User Disconnected", socket.id)
     })
